47832f4199411770540ee3c6acc26783ca43329c
Jon Ingvar Skånøy <jiskaanoy@student.uia.no>
2023-11-05 13:51:50 +0100

    test

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import cv2
 import matplotlib.pyplot as plt
 import time
 import pickle
-CUT = 1
+CUT = 2
 FRAMESKIP = 90
-videodata = cv2.VideoCapture(r"C:\Users\jonin\Desktop\machine vision\20230926_130609.mp4")
+videodata = cv2.VideoCapture(r"C:\Users\jonin\Desktop\machine vision\20230926_135215.mp4")
+
 frames = int(videodata.get(cv2.CAP_PROP_FRAME_COUNT))
 width = int(videodata.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(videodata.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -23,11 +24,12 @@ while (currentFramesCounter < frames // CUT - FRAMESKIP and ret):
     ret, buf[currentFramesCounter // FRAMESKIP] = videodata.read()
     currentFramesCounter += FRAMESKIP
     videodata.set(cv2.CAP_PROP_POS_FRAMES, currentFramesCounter)
+    print(currentFramesCounter/FRAMESKIP)
 
 
 videodata.release()
 
 
-pickle.dump(buf, open("imagess.p", "wb"))
+pickle.dump(buf, open("images3.p", "wb"))
 
 
--- test.py
+++ test.py
@@ -3,16 +3,26 @@ import cv2
 import copy
 import matplotlib.pyplot as plt
 import pickle
-IMGS = pickle.load(open("imagess.p", "rb"))
+IMGS = pickle.load(open("images2.p", "rb"))
 
-img = IMGS[300]
+img = IMGS[260]
 downscale = 1
-imggray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#imggray = cv2.pyrDown(imggray)
-#imggray = cv2.pyrDown(imggray)
 
-imgblur = cv2.GaussianBlur(imggray, (13,13), 40)
-imgblur = cv2.medianBlur(imgblur,   15)
+
+
+
+
+
+imgblur = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgblur = cv2.pyrDown(imgblur)
+imgblur = cv2.pyrDown(imgblur)
+
+imgblur = cv2.GaussianBlur(imgblur, (5,5), 10000000000)
+imgblur = cv2.medianBlur(imgblur,   3)
+
+
+
+
 
 
 #imgblur = cv2.equalizeHist(imggray)
@@ -22,27 +32,37 @@ log_image = c * (np.log(imgblur + 1))
 # Specify the data type so that
 # float value will be converted to int
 log_image = np.array(log_image, dtype=np.uint8)
+#log_image = cv2.GaussianBlur(log_image, (13,13), 40)
+#bilateral = cv2.bilateralFilter(log_image, 50, 1, 1)
+hist = cv2.calcHist([log_image],[0],None,[256],[0,256])
+th3 = cv2.adaptiveThreshold(log_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,7,1.2)
 
-bilateral = cv2.bilateralFilter(log_image, 50, 1, 1)
-hist = cv2.calcHist([bilateral],[0],None,[256],[0,256])
-th3 = cv2.adaptiveThreshold(bilateral, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,11,1)
-th3 = cv2.medianBlur(th3,   7)
+nlabels, labels, stats, centroids = cv2.connectedComponentsWithStats(th3, None, None, None, 8, cv2.CV_32S)
 
-print(len(IMGS))
-bikefiltertestTH3 = copy.deepcopy(th3)
-bikefiltertest = copy.deepcopy(img)
-for i in range(int(440/downscale),int(1080/downscale)):
-    for j in range(int(1200/downscale), int(1940/downscale)):
-       bikefiltertestTH3[i][j] = 0
-th3 = bikefiltertestTH3
-print(bilateral.max())
+#get CC_STAT_AREA component as stats[label, COLUMN]
+areas = stats[1:,cv2.CC_STAT_AREA]
+
+result = np.zeros((labels.shape), np.uint8)
+
+for i in range(0, nlabels - 1):
+    if areas[i] >= 300:   #keep
+        result[labels == i + 1] = 255
+
+#print(len(IMGS))
+#bikefiltertestTH3 = copy.deepcopy(th3)
+#bikefiltertest = copy.deepcopy(img)
+#for i in range(int(300/downscale),int(1080/downscale)):
+ #   for j in range(int(1200/downscale), int(2000/downscale)):
+  #     bikefiltertestTH3[i][j] = 0
+#th3 = bikefiltertestTH3
+print(np.sum(th3)/255)
 plt.hist(imgblur.ravel(),256,[0,256]); plt.show()
 
 #plt.imshow(th3,  cmap='gray')
 #plt.show()
 cv2.imwrite('image.png', img)
-cv2.imwrite('image_gray.png', imggray)
+#cv2.imwrite('image_gray.png', imggray)
 cv2.imwrite('image_blur.png', imgblur)
 cv2.imwrite('image_log.png',log_image)
 cv2.imwrite('image_hist.png',hist)
-cv2.imwrite('image_thresh.png', th3)
+cv2.imwrite('image_thresh.png', result)
